bf53ae647d6c6c7e76d05db50ec243ffdb7da26d
Chase W <chasew@silkweaver.com>
2016-09-24 15:12:02 -0400

    Another potential fix? This is also related to latency I think. It would explain why I can't reproduce it. Probably even more finicky thanks to receiving being on another thread.

--- StardewValleyMP/Packets/NextDayPacket.cs
+++ StardewValleyMP/Packets/NextDayPacket.cs
@@ -50,7 +50,19 @@ namespace StardewValleyMP.Packets
             ChatMenu.chat.Add(new ChatEntry(null, client.farmer.name + " is in bed."));
             server.broadcast(new ChatPacket(255, client.farmer.name + " is in bed."), client.id);
 
-            client.stage = Server.Client.NetStage.WaitingForStart;
+            // This is also set in ClientFarmerDataPacket.
+            // I think what's happening is this packet is received, and it goes to the next day.
+            // When the next day starts it sees the ClientFarmerDataPacket, and resets this same thing.
+            // So the host is ignoring everything the clients do, and queueing their messages up until
+            // whenever the stage is set back to Playing. Then all of the clients changes come out at once.
+            // The clients are oblivious and still receiving the host updates though.
+            //
+            // (Of course, I can't reproduce this since I assume it is latency related again, made even
+            // weirder with the receiving being on another thread.)
+            //
+            // Hopefully only setting this in the latter packet will fix this.
+
+            //client.stage = Server.Client.NetStage.WaitingForStart;
             if (client.farmer.currentLocation != null)
                 ;// client.farmer.currentLocation.farmers.Remove(client.farmer);
         }
