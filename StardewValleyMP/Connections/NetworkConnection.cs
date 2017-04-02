22ec79c5c8163ea81db29e9dec8a0735e9e70371
Chase W <chasew@silkweaver.com>
2017-04-02 13:34:16 -0400

    Tentative new connection interface

---
+++ StardewValleyMP/Connections/NetworkConnection.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+
+namespace StardewValleyMP.Connections
+{
+    public class NetworkConnection : IConnection
+    {
+        private TcpClient conn;
+        private NetworkStream stream;
+
+        public NetworkConnection()
+        {
+        }
+
+        ~NetworkConnection()
+        {
+            disconnect();
+        }
+
+        public void connect(String ip, int port)
+        {
+            disconnect();
+
+            TcpClient socket = new TcpClient(AddressFamily.InterNetworkV6);
+            socket.Client.DualMode = true;
+            socket.Connect(ip, port);
+            socket.NoDelay = true;
+        }
+
+        override public bool isConnected()
+        {
+            return (socket != null && socket.Connected);
+        }
+
+        override public void disconnect()
+        {
+            if (stream != null)
+            {
+                stream.Close();
+                stream = null;
+            }
+
+            if (conn != null)
+            {
+                conn.Close();
+                conn = null;
+            }
+        }
+
+        override public Stream getStream()
+        {
+            return stream;
+        }
+    }
+}
